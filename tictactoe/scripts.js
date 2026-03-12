0d1ec34234d43d48c0af5eae42982d62129f07bd
JTFS03 <44273257+JTFS03@users.noreply.github.com>
2026-03-12 05:45:29 +0000

    Copied HTML from pevious added src scripts, created board in js

---
+++ tictactoe/scripts.js
@@ -0,0 +1,65 @@
+
+
+
+let gameboard = [
+ "", "", "",
+ "", "", "",
+ "", "", ""
+];
+
+
+
+
+function checkGameboard(board){
+
+  // convert board to rows like codepen
+  let rowA = [board[0], board[1], board[2]];
+  let rowB = [board[3], board[4], board[5]];
+  let rowC = [board[6], board[7], board[8]];
+
+  let grid = [rowA, rowB, rowC];
+
+
+  // Row check
+  for (let i = 0; i < 3; i++) {
+    if (
+      grid[i][0] !== "" &&
+      grid[i][0] === grid[i][1] &&
+      grid[i][1] === grid[i][2]
+    ) {
+      return grid[i][0];
+    }
+  }
+
+
+  // Column check
+  for (let i = 0; i < 3; i++) {
+    if (
+      grid[0][i] !== "" &&
+      grid[0][i] === grid[1][i] &&
+      grid[1][i] === grid[2][i]
+    ) {
+      return grid[0][i];
+    }
+  }
+
+
+  // Diagonal check
+  if (
+    grid[0][0] !== "" &&
+    grid[0][0] === grid[1][1] &&
+    grid[1][1] === grid[2][2]
+  ) {
+    return grid[0][0];
+  }
+
+  if (
+    grid[0][2] !== "" &&
+    grid[0][2] === grid[1][1] &&
+    grid[1][1] === grid[2][0]
+  ) {
+    return grid[0][2];
+  }
+
+  return "";
+}
