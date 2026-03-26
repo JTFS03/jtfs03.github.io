20db53ae0f4336ccbe4ee6bd252d4ad3f8039d25
JTFS03 <44273257+JTFS03@users.noreply.github.com>
2026-03-26 05:16:36 +0000

    added js to flip cards on click

---
+++ Concentration/scripts.js
@@ -0,0 +1,17 @@
+//start none flipped
+let flippedCards = [];
+
+//Hey js this is what you can click on
+const cards = document.querySelectorAll(".card");
+
+//Hey js if you click on a card do this flip
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.add("flipped");
+    flippedCards.push(card);
+
+    console.log(flippedCards);
+  });
+});
+
+
